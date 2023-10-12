bcbad58b5d9d5c789a1039fef72026e1472b652d
Bruno Jardon <brunojardon3@gmail.com>
2023-10-12 14:59:57 -0300

    Archivos base API con spring boot

---
+++ src/main/java/com/tecnica/api/domain/questionService.java
@@ -0,0 +1,8 @@
+package com.tecnica.api.domain;
+import java.util.List;
+import com.tecnica.api.entity.questionEntity;
+
+public class questionService {
+    void save(Question question);
+	List<question> getQuestionList();
+}
